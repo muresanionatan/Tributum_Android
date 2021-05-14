f339564b49bae13c89df344b81a26c6f31d201eb
Ionatan Muresan <muresanionatan@gmail.com>
2021-05-14 08:53:27 +0300

    Removed unused files

--- app/src/main/java/com/example/tributum/fragment/invoices/InvoiceState.java
+++
@@ -1,9 +0,0 @@
-package com.example.tributum.fragment.invoices;
-
-import androidx.annotation.IntDef;
-
-@IntDef
-public @interface InvoiceState {
-    int PHOTO = 0,
-            PREVIEW = 1;
-}
--- app/src/main/java/com/example/tributum/utils/MailUtils.java
+++ app/src/main/java/com/example/tributum/utils/MailUtils.java
@@ -32,33 +32,6 @@ public class MailUtils {
         }
     }
 
-    private void sendEmail() {
-        Intent emailIntent = new Intent(Intent.ACTION_SEND);
-        emailIntent.setType("text/plain");
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"muresanionatan@gmail.com"});
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "subject here");
-        emailIntent.putExtra(Intent.EXTRA_TEXT, "body text");
-//        File root = Environment.getExternalStorageDirectory();
-//        String pathToMyAttachedFile = "temp/attachement.xml";
-//        File file = new File(root, pathToMyAttachedFile);
-//        if (!file.exists() || !file.canRead()) {
-//            return;
-//        }
-//        Uri uri = FileProvider.getUriForFile(
-//                getActivity(),
-//                "com.example.mbacc.fragment.provider", //(use your app signature + ".provider" )
-//                signature);
-//        emailIntent.putExtra(Intent.EXTRA_STREAM, uri);
-//
-//        List<ResolveInfo> resInfoList = getActivity().getPackageManager().queryIntentActivities(emailIntent, PackageManager.MATCH_DEFAULT_ONLY);
-//        for (ResolveInfo resolveInfo : resInfoList) {
-//            String packageName = resolveInfo.activityInfo.packageName;
-//            getActivity().grantUriPermission(packageName, uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-//        }
-//
-//        getActivity().startActivity(Intent.createChooser(emailIntent, "Pick an Email provider"));
-    }
-
     public static boolean isEmailValid(CharSequence email) {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
