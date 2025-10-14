1d62f6a0fec7dfe78fa378e6dae933fa710ba808
muresanionatan <muresanionatan>
2025-10-14 22:35:20 +0300

    Update form screen

--- app/src/main/java/com/app/tributum/activity/form/FormActivity.java
+++ app/src/main/java/com/app/tributum/activity/form/FormActivity.java
@@ -1,6 +1,8 @@
 package com.app.tributum.activity.form;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.app.tributum.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class FormActivity extends AppCompatActivity {
+
+    private BottomSheetBehavior<View> fileChooser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_activity);
+
+        RelativeLayout llBottomSheet = findViewById(R.id.file_chooser_id);
+        fileChooser = BottomSheetBehavior.from(llBottomSheet);
+        fileChooser.setDraggable(false);
     }
 }
